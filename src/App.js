c7191e6c5c92c8d9cc863f99cb510b68eca3f59c
Mison Caamwang <mison.caamwang@hotmail.com>
2019-12-11 00:27:15 +1100

    new

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import './App.css';
-import Todos from './components/Todos'
+import TodoList from './TodoList';
 
+//This is the root. Class based component. App is able to inherit all the methonds from React.Componenet (React was imported in line one) object because its a sub-class
 class App extends React.Component {
-  // State is getting passed to Todo component here. You can acess this in Todos.js as props
-  state = {
-    todo: [
-      {
-        id: 1,
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Learn React',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Finish dinner',
-        completed: false
-      }
-    ]
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Todos todos={this.state.todo}/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<TodoList />
+			</div>
+		  )
+	}
 }
 
 export default App;
