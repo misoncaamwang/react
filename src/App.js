f6824dfaf0a6997a17bbd2ee13d67ae94b68aee5
Mison Caamwang <mison.caamwang@hotmail.com>
2019-12-18 17:02:38 +1100

    New

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import TodoList from './TodoList';
 
-//This is the root. Class based component. App is able to inherit all the methonds from React.Componenet (React was imported in line one) object because its a sub-class
 class App extends React.Component {
 	render() {
-		return (
-			<div>
-				<TodoList />
+		return(
+			<div className="App">
+				<h1>Sorting Algorithm Visualiser</h1>
+				
 			</div>
-		  )
+		)
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // This is the main React Librart
+import ReactDOM from 'react-dom'; // This injects react to the DOM.
 import App from './App';
 
-//React starts in this file. App is a componenet which is rendered. 
+// Can use bable to compile jsx to javascript
+// React starts in this file. App is a componenet which is rendered. 
+// ReactDOM is needed to render all the components.
 ReactDOM.render(<App />, document.getElementById('root'));
